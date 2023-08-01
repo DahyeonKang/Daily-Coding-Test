ae324670ddfe933fee43a9b7c087ca7a7df20e41
DahyeonKang <kl1223k@naver.com>
2023-08-02 02:01:23 +0900

    Lv11. 시간 복잡도 - 알고리즘의 수행 시간 4~6

--- Baekjoon/Lv11. 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 1~6.py
+++ Baekjoon/Lv11. 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 1~6.py
@@ -53,5 +53,44 @@ print(2)
 #     return sum;
 # }
 n = int(input())
+print(int(n*(n-1)/2))
+print(2)
+
+# 다른 분 코드
+n=int(input())
+print(n*(n-1)//2, 2, sep='\n')
+# 다른 붙 코드
+a = int(input())
+print((a*(a-1))//2)
+print(2)
+
+
+## 알고리즘 수업 - 알고리즘의 수행 시간 5
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             for k <- 1 to n
+#                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#     return sum;
+# }
+n = int(input())
+print(n**3)
+print(3)
+
+
+## 알고리즘 수업 - 알고리즘의 수행 시간 6
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n - 2
+#         for j <- i + 1 to n - 1
+#             for k <- j + 1 to n
+#                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#     return sum;
+# }
+n = int(input())
+print()
+print(3)
+
 
 
