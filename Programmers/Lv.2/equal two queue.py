8906acd6ddf1a46427882e9e207799820f520abb
KangDa <kl1223k@naver.com>
2024-01-11 14:58:18 +0900

    백준 Lv.15, 프로그래머스 Lv.2, SQL

---
+++ Programmers/Lv.2/equal two queue.py
@@ -0,0 +1,13 @@
+# lv.2 : 두 큐 합 같게 만들기
+
+# 길이가 같은 두 개의 큐 주어짐
+# pop + insert 가 횟수 1번으로 간주함
+
+def mysolution(queue1, queue2):
+    answer = -2
+    return answer
+
+
+print(mysolution([3, 2, 7, 2], [4, 6, 5, 1]))
+print(mysolution([1, 2, 1, 2], [1, 10, 1, 2]))
+print(mysolution([1, 1], [1, 5]))
