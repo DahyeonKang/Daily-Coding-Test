4497e1128c77e6005835d3bb35066070267c272f
KangDa <kl1223k@naver.com>
2024-02-01 22:23:48 +0900

    정렬 - lv.2

--- Programmers/정렬/H-Index.py
+++ Programmers/정렬/H-Index.py
@@ -1,17 +1,20 @@
 ## 문제 : H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를 구하려고 합니다.
 
 
+# 성공 2
 def solution(citations):
-    answer = -1
-    h = 0
-    count = 0
     n = len(citations)
-    citations.sort(reverse=True)
-    while h == n-1:
-        h += 1
-        if h == citations[h-1]:
-            answer = h
+    for h in range(max(citations), -1, -1):
+        higher = sum([1 if i >= h else 0 for i in citations])
+        if higher >= h and (n-higher) <= h:
             return h
-    return answer
 
-print(solution([3, 0, 6, 1, 5]))
+
+# 다른 분 코드
+def solution(citations):
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
