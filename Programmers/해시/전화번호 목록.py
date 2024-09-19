22958e2976a84ef673b5b10ed1a5eabb95261c67
KangDa <kl1223k@naver.com>
2024-09-20 00:42:55 +0900

    파일 위치 수정

--- Programmers/해시/전화번호 목록.py
+++ Programmers/해시/전화번호 목록.py
@@ -16,9 +16,11 @@ def solution(phone_book):
 def solution(phoneBook):
     phoneBook = sorted(phoneBook)
     for p1, p2 in zip(phoneBook, phoneBook[1:]):
-        if p2.startswith(p1):
+        if p2.startswith(p1):   # startswith : 괄호 안의 문자열이 앞 문자열의 접두사인지 확인해 boolean으로 반환
+                                # endswith :               ''             접미사인지 확인해 boolean으로 변환
             return False
     return True
+
 
 # 정렬 사용한 코드
 # 내가 이전에 했던 코드 실패 이유 : 문자열 sort는 숫자 정렬처럼 정렬되는 줄 알고, key값으로 첫번째 값 기준으로만 정렬했다.
@@ -28,6 +30,17 @@ def solution(phone_book):
         if phone_book[i]==phone_book[i+1][:len(phone_book[i])]:
             return False
     return True
+
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if len(phone_book[i]) < len(phone_book[i+1]):
+            if phone_book[i]==phone_book[i+1][:len(phone_book[i])]:
+                return False
+    return True
+
+
 
 # hash 이용한 코드
 def solution(phone_book):
